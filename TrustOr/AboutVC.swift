e1e2d3782757bede2627a5800899b0a93f8cfba4
Dmitry Dementyev <dmitryd@bk.ru>
2018-09-03 20:50:34 +0300

    add table

---
+++ TrustOr/AboutVC.swift
@@ -0,0 +1,30 @@
+//
+//  AboutVC.swift
+//  Верю-Не-верю
+//
+//  Created by Dmitry Dementyev on 03.09.2018.
+//  Copyright © 2018 Dmitry Dementyev. All rights reserved.
+//
+
+import UIKit
+
+class AboutVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
